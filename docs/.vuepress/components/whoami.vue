61061fac3ac90518bf8965b6c9c3b4e11d351ddb
Lennon Day-Reynolds <lennon@rcoder.net>
2019-10-22 22:22:36 -0700

    fix build

--- docs/.vuepress/components/whoami.vue
+++ docs/.vuepress/components/whoami.vue
@@ -2,21 +2,17 @@
   <form id="whoami">
     <fieldset v-if="!alias">
       <input type="text" v-model="alias" placeholder="pick an alias..." />
-      <input type="password" v-model="[passphrase]" placeholder="...and a passphrase"
-        @keyup.enter= />
+      <input type="password" v-model="passphrase" placeholder="...and a passphrase" />
     <Badge v-if="alias">online</Badge>
     <Badge type="error" v-if="!alias">offline</Badge>
   </form>
 </template>
 
 <script>
-export const passphrase = Symbol.for('passphrase')
-
 export default {
-  name: 'whoami',
-  props: {
-    alias: string,
-    [passphrase]: string
-  }
+  data: () => ({
+    text: '',
+    password: ''
+  })
 }
 </script>
