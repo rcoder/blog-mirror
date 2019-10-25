5a73fd521c9123b2c3e778eac379a603fcf262d3
Lennon Day-Reynolds <lennon@rcoder.net>
2019-10-25 16:32:40 -0700

    comments!

---
+++ docs/.vuepress/components/comments.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="comments">
+    <blockquote v-for="comment in comments" class="one-comment">
+      {{ comment.message }}
+      <br/>
+      <sub>{{ new Date(comment.createdAt).toDateString() }} by {{ comment.from }}</sub>
+    </blockquote>
+    <form @submit.prevent="onClick">
+      <input type="email" v-model="email" placeholder="Email" />
+      <input type="text" v-model="message" placeholder="Say something..." />
+      <input type="submit" value="post" />
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import moment from 'moment'
+
+const COMMENT_BASE = 'https://bc8.org/_api/comments'
+const commentPage = (page) => `${COMMENT_BASE}/${encodeURIComponent(page.path.replace(/^\/+|\/+$/g, ''))}`
+
+export default {
+  data() {
+    return {
+      comments: [],
+      email: '',
+      message: '',
+    }
+  },
+  async beforeMount() {
+    let result = await axios.get(commentPage(this.$page))
+    this.$data.comments = result.data
+  },
+  methods: {
+    async onClick(event) {
+      let result = await axios.post(commentPage(this.$page), {
+        from: this.$data.email,
+        message: this.$data.message
+      })
+
+      this.$data.comments = result.data
+    }
+  }
+}
+</script>
--- server.ts
+++ server.ts
@@ -1,36 +1,96 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
+import cors from 'fastify-cors'
+import nedb from 'nedb-promises'
 
-const sse = require('fastify-sse');
+import path from 'path'
 
-declare module 'fastify' {
-  interface FastifyReply<HttpResponse> {
-    sse: (_:any) => any;
+const server = fastify({
+  logger: {
+    level: 'debug'
   }
+})
+
+server.register(cors, { origin: '*' })
+
+const PRESENCE_TTL = 600
+const PAGE_ACTIVITY_TTL = 3600 * 72
+
+function openDb(name: string) {
+  return nedb.create({
+    filename: path.join(__dirname, 'data', `${name}.db`),
+    autoload: true,
+    timestampData: true
+  })
 }
 
-const server = fastify();
-server.register(sse);
+function cleanPath(path: string) {
+  return decodeURIComponent(path).replace(/^\/+|\/+$/g, '')
+}
 
-const ttl = 600;
+const comments = openDb('comments')
 
-const online = new Map<string, Date>();
-setTimeout(() => {
+const presence = openDb('presence')
+presence.ensureIndex({
+  fieldName: 'updatedAt',
+  expireAfterSeconds: PRESENCE_TTL
+} as any)
 
-}, 1000);
+const pages = openDb('activity')
+pages.ensureIndex({
+  fieldName: 'updatedAt',
+  expireAfterSeconds: PAGE_ACTIVITY_TTL
+} as any)
+
+server.route({
+  method: 'GET',
+  url: '/comments/:path',
+  handler: async (req, reply) => {
+    let path = cleanPath(req.params.path)
+    let results = await comments.find({path}).sort({createdAt: -1}).limit(50).exec()
+    server.log.debug('comments')
+    server.log.debug(results)
+    reply.send(results)
+  }
+})
+
+server.route({
+  method: 'POST',
+  url: '/comments/:path',
+  handler: async (req, reply) => {
+    let path = cleanPath(req.params.path)
+
+    await pages.update({path}, {lastCommentAt: new Date()}, {upsert: true})
+    await comments.insert({
+      path: path,
+      ...req.body
+    })
+    reply.send(await comments.find({path}))
+  }
+})
 
 server.route({
   method: 'GET',
   url: '/presence',
-  handler: (req, reply) => {
-    reply.sse(Array(online));
+  handler: async (req, reply) => {
+    reply.send(await presence.find({}))
   }
-});
+})
 
 server.route({
   method: 'POST',
   url: '/presence',
-  handler: (req, reply) => {
-    const alias = req.body.alias;
-    online.set(alias, new Date());
+  handler: async (req, reply) => {
+    let updated = await presence.update(
+      {
+        alias: req.body.alias,
+        path: req.body.path
+      },
+      req.body,
+      {
+        upsert: true
+    })
+    reply.send(updated)
   }
-});
+})
+
+server.listen(process.env.PORT ? parseInt(process.env.PORT) : 8081)
